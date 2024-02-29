ea7401e8f5d7f25ff83c42feff674579f2d999bb
Vincent MARAIS <80059002+Betawolfy@users.noreply.github.com>
2024-02-29 08:25:51 +0100

    Create MyMainClass.java

---
+++ Cochon/MyMainClass.java
@@ -0,0 +1,7 @@
+public class MyMainClass {
+    public static void main(String[] args) {
+    Cochon monCochon = new Cochon(); // crée un objet Cochon
+    monCochon.son();
+    monCochon.sleep();
+    }
+}
